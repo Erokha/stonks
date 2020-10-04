964674e34538e50bc3aa083e75b03d28e6845675
Vlad <vkrivozubov@list.ru>
2020-10-04 21:03:40 +0300

    ADD: welcome image for account view

--- Stonks/Stonks/Views/LoginViewController.swift
+++ Stonks/Stonks/Views/LoginViewController.swift
@@ -11,10 +11,42 @@ class LoginViewController: UIViewController {
 
     var viewModel: LoginViewModelType?
     
+    private lazy var welcomeImage: UIImageView = {
+        let image = UIImageView()
+        let assetName = "welcome"
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: assetName)
+        image.contentMode = .scaleAspectFill
+        
+        return image
+    }()
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+    
+    private func addSubviews() {
+        view.addSubview(welcomeImage)
+    }
+    
+    private func setupWelcomeImageConstraints() {
+        let topConstant: CGFloat = 150
+        
+        welcomeImage.topAnchor.constraint(equalTo: view.topAnchor, constant: topConstant).isActive = true
+        welcomeImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setupConstraints() {
+        setupWelcomeImageConstraints()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        setupView()
+        addSubviews()
+        setupConstraints()
     }
 
 
