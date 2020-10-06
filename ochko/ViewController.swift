cb8f8217cc76313b12b6f3e80ff971db6982a1d9
kymblc <coffeemilk1337@gmail.com>
2020-10-06 18:37:02 +0300

    check this out

---
+++ ochko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ochko
+//
+//  Created by kymblc on 06.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myView: CardView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
