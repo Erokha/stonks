ef7824eb5f2c54fea4e6241d74eca06817b7b4b7
Zakharov Alexandr <alexanzakharov@gmail.com>
2020-10-06 16:42:14 +0300

    FIX:: HEADER VIEW CHANGED TO  CONTAINER VIEW

---
+++ Stonks/Stonks/Views/Me/HeaderViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HeaderViewController.swift
+//  Stonks
+//
+//  Created by  Alexandr Zakharov on 06.10.2020.
+//
+
+import UIKit
+
+class HeaderViewController: UIViewController {
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUi()
+
+    }
+}
+
+
+extension HeaderViewController
+{
+    func configureUi(){
+        self.headerView.layer.cornerRadius = 20
+        self.headerView.layer.shadowPath = UIBezierPath(rect: headerView.bounds).cgPath
+        self.headerView.layer.shadowRadius = 20
+        self.headerView.layer.shadowOffset = .zero
+        self.headerView.layer.shadowOpacity = 0.8
+        self.profileImage.image = UIImage(named: "ZUEV")
+        self.profileImage.layer.cornerRadius = 15
+        
+    }
+}
--- Stonks/Stonks/Views/Me/MePortfolioViewController.swift
+++ Stonks/Stonks/Views/Me/MePortfolioViewController.swift
@@ -8,31 +8,8 @@
 import UIKit
 
 class MePortfolioViewController: UIViewController {
-    @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var lastNameLabel: UILabel!
-    
     override func viewDidLoad() {
-        
-        nameLabel.text = "Nikita"
-        lastNameLabel.text = "Juev"
         super.viewDidLoad()
-        self.configureUi()
     }
 }
 
-
-extension MePortfolioViewController
-{
-    func configureUi(){
-        self.headerView.layer.cornerRadius = 20
-        self.headerView.layer.shadowPath = UIBezierPath(rect: headerView.bounds).cgPath
-        self.headerView.layer.shadowRadius = 20
-        self.headerView.layer.shadowOffset = .zero
-        self.headerView.layer.shadowOpacity = 0.8
-        self.profileImage.image = UIImage(named: "ZUEV")
-        self.profileImage.layer.cornerRadius = 15
-        
-    }
-}
