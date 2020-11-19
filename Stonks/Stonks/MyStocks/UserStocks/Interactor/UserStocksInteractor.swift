e49ffcee0f3366a9a5dfb27dda1131d983f9e75a
kymblc <coffeemilk1337@gmail.com>
2020-11-19 22:20:51 +0300

    [ADD] user stocks FRC

--- Stonks/Stonks/MyStocks/UserStocks/Interactor/UserStocksInteractor.swift
+++ Stonks/Stonks/MyStocks/UserStocks/Interactor/UserStocksInteractor.swift
@@ -1,8 +1,39 @@
 import Foundation
 import Alamofire
+import CoreData
 
-final class UserStocksInteractor {
+final class UserStocksInteractor: NSObject {
     weak var output: UserStoksInteractorOutput?
+    private var frc: NSFetchedResultsController<Stock>?
+
+    required override init() {
+        super.init()
+        configureFrc()
+        frc?.delegate = self
+    }
+
+    func configureFrc() {
+        let fetchRequest: NSFetchRequest<Stock> = Stock.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataService.shared.getPersistentContainer().viewContext, sectionNameKeyPath: nil, cacheName: nil)
+
+        do {
+            try frc?.performFetch()
+        } catch {
+            fatalError("Error while performing fetch")
+        }
+    }
+
+    private func prepareModels(for fetchResult: [NSFetchRequestResult]) -> [StockData] {
+        if let object = fetchResult as? [Stock] {
+            let stocks: [StockData] = object.map({ StockData(with: $0) })
+            return stocks
+        } else {
+            return []
+        }
+    }
 
     private func handleError(with error: Error) {
         switch error.localizedDescription {
@@ -42,5 +73,15 @@ extension UserStocksInteractor: UserStoksInteractorInput {
             }
             self?.handleStock(with: stocks)
         })
+    }
+}
+
+extension UserStocksInteractor: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        if let fetchResult = controller.fetchedObjects {
+            let stocks = prepareModels(for: fetchResult)
+            output?.didReciveCoreData(stocks: stocks)
+            //output?.didChangeContetnt(stocks: )
+        }
     }
 }
