b1de9c943d775da21c70ed2a105e6265336991b4
Vlad <vkrivozubov@list.ru>
2020-11-20 22:55:14 +0300

    [FIX] CoreData

--- Stonks/Stonks/StockDetail/Model/StockDetailData.swift
+++ Stonks/Stonks/StockDetail/Model/StockDetailData.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct StockDetailPresenterData {
+struct StockPresenterData {
     var name: String?
     var symbol: String?
     var amount: Int?
@@ -8,7 +8,7 @@ struct StockDetailPresenterData {
 
     var cardData: CardData?
 
-    init(model: Stock) {
+    init(model: StockInteractorData) {
         self.name = model.name
         self.symbol = model.symbol
         self.amount = model.amount
@@ -21,6 +21,30 @@ struct StockDetailPresenterData {
 
     init() {
 
+    }
+}
+
+struct StockInteractorData {
+    var name: String
+    var symbol: String
+    var totalCost: NSDecimalNumber
+    var amount: NSInteger
+    var priceHistory: [NSDecimalNumber]
+
+    init(stock: Stock) {
+        self.name = stock.name
+        self.symbol = stock.symbol
+        self.totalCost = stock.totalCost
+        self.amount = stock.amount
+        self.priceHistory = stock.priceHistory
+    }
+
+    init(name: String, symbol: String) {
+        self.name = name
+        self.symbol = symbol
+        self.totalCost = 0
+        self.amount = 0
+        self.priceHistory = []
     }
 }
 
