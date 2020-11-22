bc1b405c419fdd37c27268c046be1d2720e7e4c9
kymblc <coffeemilk1337@gmail.com>
2020-11-22 21:20:44 +0300

    added pretty icons

--- Stonks/Stonks/MyStocks/MyStocksShared/TableViewCell/StockTableViewCell.swift
+++ Stonks/Stonks/MyStocks/MyStocksShared/TableViewCell/StockTableViewCell.swift
@@ -9,17 +9,28 @@ class StockTableViewCell: UITableViewCell {
     @IBOutlet private weak var stockCountLabel: UILabel!
     @IBOutlet private weak var arrowButton: UIButton!
     @IBOutlet private weak var newsImageView: UIImageView!
+    @IBOutlet private weak var container: UIView!
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
 
     func setData(data: StockData) {
+        self.setShadow()
         self.stockNameLabel.text = data.stockName
         self.stockPriceLabel.text = String(data.stockPrice) + "$"
         self.stockCountLabel.text = "you own: \(data.stockCount)"
         let url = URL(string: data.imageUrl)
         self.newsImageView.kf.setImage(with: url)
+    }
+
+    private func setShadow() {
+        self.container.layer.cornerRadius = 15
+        self.container.layer.shadowColor = UIColor.gray.cgColor
+        self.container.layer.shadowOffset = .init(width: 0, height: 2)
+        self.container.layer.shadowOpacity = 0.4
+        self.layoutMargins = UIEdgeInsets.zero
+        self.preservesSuperviewLayoutMargins = false
     }
 
 }
