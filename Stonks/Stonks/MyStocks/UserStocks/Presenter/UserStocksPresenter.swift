b0e35a8531d1ebfc304a99b337cbd806f7a60f52
Vlad <vkrivozubov@list.ru>
2020-12-15 18:55:06 +0300

    [FIX] myStocks

--- Stonks/Stonks/MyStocks/UserStocks/Presenter/UserStocksPresenter.swift
+++ Stonks/Stonks/MyStocks/UserStocks/Presenter/UserStocksPresenter.swift
@@ -71,7 +71,7 @@ extension UserStocksPresenter: UserStocksViewOutput {
 extension UserStocksPresenter: UserStoksInteractorOutput {
     func didReciveUpdate(userStockUpdate: [String: (Float, String)]) {
         guard var data = self.model else { return }
-        for i in 0...data.count - 1 {
+        for i in 0..<data.count {
             data[i].stockPrice = userStockUpdate[data[i].stockSymbol]?.0 ?? 0
             data[i].imageUrl = userStockUpdate[data[i].stockSymbol]?.1 ?? "not found"
         }
